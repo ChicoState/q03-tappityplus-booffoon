9efc9f8662cf0a5a29ad412482335e63be4a6ca8
Jesse Coulson <jccoulson@mail.csuchico.edu>
2022-12-05 00:02:14 -0800

    working tappityplus for quiz 3

--- tappity.cpp
+++ tappity.cpp
@@ -6,12 +6,17 @@
 //Constructor sets the reference phrase
 tappity::tappity(std::string reference)
 {
+  //set ref to given refernce and inp to empty string
+  ref = reference;
+  inp = "";
 }
 
-//Provide the input to be compared to the reference. Before this 
+//Provide the input to be compared to the reference. Before this
 //function is called, the input should be considered an empty string
 void tappity::entry(std::string input)
 {
+  //set inp to user entered input
+  inp = input;
 }
 
 //Compares the length of the reference to that of the input and
@@ -19,16 +24,18 @@ void tappity::entry(std::string input)
 //same length
 int tappity::length_difference()
 {
-  return 0;
+  int difference = ref.length() - inp.length();
+  difference = abs(difference);
+  return difference;
 }
 
 //Compares the content of the reference to that of the input and
 //calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
+//are identical, accuracy should be 100. However, for each
 //corresponding character that does not match in the same location,
 //the percentage of corresponding characters should be returned.
 //For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
+//matches 8 of the corresponding reference characters, the function
 //should return 80. When input does not have the same number of
 //characters as the reference, the accuracy should represent the percent
 //of matching characters between the shorter and longer strings. For
@@ -36,5 +43,47 @@ int tappity::length_difference()
 //locations in another string that has 16 characters, the accuracy is 50.
 double tappity::accuracy()
 {
-  return 0;
+
+  //track how many characters amtch
+  double match = 0;
+  //iterate through length of longest string to find how many characters match
+  std::string longer = "";
+  if(ref.length() > inp.length())
+  {
+    longer = "ref";
+    for(int i = 0; i < ref.length(); i ++)
+    {
+      if(ref[i] == inp[i])
+          match++;
+    }
+  }
+  else
+  {
+    longer = "inp";
+    for(int i = 0; i < inp.length(); i ++)
+    {
+      if(ref[i] == inp[i])
+          match++;
+    }
+  }
+
+  double matchPercent = 0.0;
+
+  if(ref.length() == 0 && inp.length() == 0) //if both strings are empty it is a 100% match
+  {
+    matchPercent = 100;
+  }
+  else if(longer == "ref") //if ref is longer find the percentage match out of ref
+  {
+    matchPercent = match/ref.length();
+    matchPercent = matchPercent * 100;
+  }
+  else if(longer == "inp") //if inp is longer find the percentage match out of inp
+  {
+    matchPercent = match/inp.length();
+    matchPercent = matchPercent * 100;
+  }
+
+  //return final percentage
+  return matchPercent;
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -4,12 +4,186 @@
 
 #include <gtest/gtest.h>
 #include "tappity.h"
- 
+
 class tappityTest : public ::testing::Test
 {
 	protected:
-		tappityTest(){} 
-		virtual ~tappityTest(){} 
+		tappityTest(){}
+		virtual ~tappityTest(){}
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
+
+TEST(tappityTest, smoketest)
+{
+	ASSERT_TRUE(true);
+}
+
+
+TEST(tappityTest, length_both_empty)
+{
+  tappity tap("");
+  tap.entry("");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 0);
+}
+
+TEST(tappityTest, length_both_same)
+{
+  tappity tap("asd");
+  tap.entry("fgh");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 0);
+}
+
+TEST(tappityTest, length_reference_larger)
+{
+  tappity tap("12345");
+  tap.entry("67");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 3);
+}
+
+TEST(tappityTest, length_input_larger)
+{
+  tappity tap("12");
+  tap.entry("34567");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 3);
+}
+
+TEST(tappityTest, length_reference_empty)
+{
+  tappity tap("");
+  tap.entry("asdf");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 4);
+}
+
+TEST(tappityTest, length_input_empty)
+{
+  tappity tap("asdf");
+  tap.entry("");
+  int difference = tap.length_difference();
+	ASSERT_EQ(difference, 4);
+}
+
+TEST(tappityTest, accuracy_both_empty)
+{
+  tappity tap("");
+  tap.entry("");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 100);
+}
+
+TEST(tappityTest, accuracy_input_empty)
+{
+  tappity tap("asd");
+  tap.entry("");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 0.0);
+}
+
+TEST(tappityTest, accuracy_reference_empty)
+{
+  tappity tap("");
+  tap.entry("asd");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 0.0);
+}
+
+TEST(tappityTest, accuracy_one_character_same)
+{
+  tappity tap("a");
+  tap.entry("a");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 100);
+}
+
+TEST(tappityTest, accuracy_one_character_different)
+{
+  tappity tap("a");
+  tap.entry("b");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 0.0);
+}
+
+TEST(tappityTest, accuracy_string_full_match)
+{
+  tappity tap("test");
+  tap.entry("test");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 100);
+}
+
+TEST(tappityTest, accuracy_string_no_match)
+{
+  tappity tap("1234");
+  tap.entry("test");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 0);
+}
+
+TEST(tappityTest, accuracy_input_larger)
+{
+  tappity tap("a");
+  tap.entry("asdf");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 25.0);
+}
+
+TEST(tappityTest, accuracy_reference_larger)
+{
+  tappity tap("asdf");
+  tap.entry("a");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 25.0);
+}
+
+TEST(tappityTest, accuracy_all_repeating_same)
+{
+  tappity tap("aaaaaaaaaa");
+  tap.entry("aaaaaaaaaa");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 100);
+}
+
+TEST(tappityTest, accuracy_all_repeating_different)
+{
+  tappity tap("aaaaaaaaaa");
+  tap.entry("bbbbbbbbbb");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 0);
+}
+
+TEST(tappityTest, accuracy_begginning_matching)
+{
+  tappity tap("ddasasasas");
+  tap.entry("ddopopopop");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 20);
+}
+
+TEST(tappityTest, accuracy_middle_matching)
+{
+  tappity tap("asasddasas");
+  tap.entry("opopddopop");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 20);
+}
+
+TEST(tappityTest, accuracy_ending_matching)
+{
+  tappity tap("asasasasdd");
+  tap.entry("opopopopdd");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 20);
+}
+
+TEST(tappityTest, accuracy_long_words)
+{
+  tappity tap("aaaaehhhheppppeuuuuee");
+  tap.entry("xxxxebbbbennnnemmmmllllee");
+  double accuracyPercent = tap.accuracy();
+	ASSERT_EQ(accuracyPercent, 12);
+}
